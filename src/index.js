c7ec47f04db5888908c6c90f1cc15346139937df
dhanush <11dhanushs@gmail.com>
2022-01-27 17:43:35 +0530

    Create Static website for book listing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import reactDom from 'react-dom'
+import './index.css'
+// always return JSX
+// stateless = dumb
+function BookList() {
+  return (
+    <section className='bookList'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+const Image = () => (
+  <img
+    src='https://images-eu.ssl-images-amazon.com/images/I/51q1eW5qcsL._AC_SX184_.jpg'
+    alt=''
+    srcset=''
+  />
+)
+const Title = () => <h1>The White Tiger</h1>
+const Author = () => <h2>Aravinda Adiga</h2>
+reactDom.render(<BookList />, document.getElementById('root'))
